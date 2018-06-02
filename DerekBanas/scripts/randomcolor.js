182875d0c67ac02f384075c923c5d2be1f22a5f9
willystyle7 <ilia111@yahoo.com>
2018-06-02 16:39:03 +0300

    exp

---
+++ DerekBanas/scripts/randomcolor.js
@@ -0,0 +1,12 @@
+function init() {
+    var h1tags = document.getElementsByTagName("h1");
+    h1tags[0].onclick = changeColor;
+}
+
+function changeColor() {
+    this.innerHTML = "Click Again";
+    var randomcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    this.style.color = randomcolor;
+}
+
+onload = init;
