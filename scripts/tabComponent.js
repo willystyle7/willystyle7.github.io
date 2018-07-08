d86002f48ab5ae9f411d6046cc952cec6841a8cb
willystyle7 <ilia111@yahoo.com>
2018-07-08 17:43:15 +0300

    tabs component

--- scripts/tabComponent.js
+++ scripts/tabComponent.js
@@ -1,7 +1,64 @@
+Vue.component('tabs', {
+    template: `
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active':tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)"> {{tab.name}} </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            tabs: []
+        };
+    },
+    created() {
+        this.tabs = this.$children;
+    },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name)
+            });
+        }
+    }
+});
 
+Vue.component('tab', {
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+    props: {
+        name: {
+            required: true
+        },
+        selected: {
+            default: false
+        }
+    },
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/\s+/g, '-');
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    },
+});
 
 
 
 new Vue({
-    el: 'root'
+    el: '#root'
 });
