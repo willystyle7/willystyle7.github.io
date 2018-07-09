aa19e1808dd9dbd97267a43eacec195352e4e9fd
willystyle7 <ilia111@yahoo.com>
2018-07-09 20:28:39 +0300

    Questionare

--- scripts/questionComponent.js
+++ scripts/questionComponent.js
@@ -7,31 +7,29 @@ Vue.component('question', {
     props: ['question'],
     // data() {
     //     return {
-    //         isVisible: true
+    //         picked: ''
     //     };
     // },
     template: `
-        <div class="control">
-            <h3>{{question.question}}<h3/>
-            <label class="radio">
-                <input type="radio" name="answer">
-                {{question.answer1}}
-            </label><br/>            
-            <label class="radio">
-                <input type="radio" name="answer">
-                {{question.answer2}}
-            </label><br/>
-            <label class="radio">
-                <input type="radio" name="answer">
-                {{question.answer3}}
-            </label><br/>
-            <hr/> 
-        </div>           
+        <form>
+            <div class="control">
+                <h3>{{question.question}}</h3>            
+                <ul>
+                    <li v-for="answer in question.answers">
+                        <label class="radio">
+                            <input type="radio" name="answer" :value="answer" v-model="question.picked">
+                            <span>{{answer}}</span>
+                        </label> 
+                    </li>
+                </ul>
+                <br/>
+                <span>Chosen answer: {{question.picked}}</span>
+                <hr/>
+            </div>    
+        </form>       
     `,
     methods: {
-        hideMessage() {
-            this.message.isVisible = false;
-        }
+
     }
 });
 
@@ -40,61 +38,50 @@ var app = new Vue({
     data: {
         questions: [{
                 question: 'What is your name?',
-                answer1: 'Ilia',
-                answer2: 'Filia',
-                answer3: 'Lilia',
-                correct: 'answer1'
+                answers: ['Ilia', 'Filia', 'Lilia', 'Kilia'],
+                correct: 'Ilia',
+                picked: ''
             },
             {
                 question: 'What is your age?',
-                answer1: '14',
-                answer2: '23',
-                answer3: '44',
-                correct: 'answer3'
+                answers: ['14', '24', '44', ],
+                correct: '44',
+                picked: ''
             },
             {
                 question: 'What is your town?',
-                answer1: 'Smilian',
-                answer2: 'Smolian',
-                answer3: 'Sofia',
-                correct: 'answer2'
+                answers: ['Smilian', 'Smolian', 'Sofia', 'Seatle'],
+                correct: 'Smolian',
+                picked: '',
             },
             {
                 question: 'What is your purpose?',
-                answer1: 'failure',
-                answer2: 'success',
-                answer3: 'fraud',
-                correct: 'answer2'
+                answers: ['failure', 'success', 'fraud'],
+                correct: 'success',
+                picked: ''
             }
         ],
+        result: '',
         questionEntry: '',
-        answer1Entry: '',
-        answer2Entry: '',
-        answer3Entry: '',
+        answerEntry: '',
         correctEntry: ''
     },
+    computed: {
+        resultProcentage() {
+            return (this.result / this.questions.length * 100);
+        }
+    },
     methods: {
-        showAll() {
-            for (let i = 0; i < this.messages.length; i++) {
-                this.messages[i].isVisible = true;
+        calculateResult() {
+            this.result = 0;
+            for (let i = 0; i < this.questions.length; i++) {
+                if (this.questions[i].picked === this.questions[i].correct) {
+                    this.result++;
+                }
             }
+            return this.result;
         },
         addQuestion() {
-            if (this.questionEntry != '' && this.answer1Entry != '' &&
-                this.answer2Entry != '' && this.answer3Entry != '' && this.correctEntry != '') {
-                this.questions.push({
-                    title: this.messageTitle,
-                    body: this.messageBody,
-                    isVisible: true
-                });
-                this.questionEntry = '';
-                this.answer1Entry = '';
-                this.answer2Entry = '';
-                this.answer3Entry = '';
-                this.correctEntry = '';
-            } else {
-                alert('Please Enter All Question Fields!');
-            }
 
         }
     }
